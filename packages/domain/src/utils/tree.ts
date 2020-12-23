036443ed8d07f3e71794d707ef5d5d94930e6e09
scottlee309 <scottlee309@gmail.com>
2020-12-23 15:10:27 +1100

    updated replace to replaceAll

--- packages/domain/src/utils/tree.ts
+++ packages/domain/src/utils/tree.ts
@@ -6,33 +6,33 @@ export const createInTree = (
   tree: Tree,
   path: string,
   content: Buffer | string
-): void => tree.create(path.replace('/', sep), content);
+): void => tree.create(path.replace(/\//g, sep), content);
 
 export const deleteInTree = (tree: Tree, path: string): void => {
-  tree.delete(path.replace('/', sep));
+  tree.delete(path.replace(/\//g, sep));
 };
 
 export const readInTree = (tree: Tree, path: string): Buffer =>
-  tree.read(path.replace('/', sep));
+  tree.read(path.replace(/\//g, sep));
 
 export const existsInTree = (tree: Tree, path: string): boolean =>
-  tree.exists(path.replace('/', sep));
+  tree.exists(path.replace(/\//g, sep));
 
 export const renameInTree = (
   tree: Tree,
   fromPath: string,
   toPath: string
 ): void => {
-  tree.rename(fromPath.replace('/', sep), toPath.replace('/', sep));
+  tree.rename(fromPath.replace(/\//g, sep), toPath.replace(/\//g, sep));
 };
 
 export const getDirInTree = (tree: Tree, path: string): any =>
-  tree.getDir(path.replace('/', sep));
+  tree.getDir(path.replace(/\//g, sep));
 
 export const overwriteInTree = (
   tree: Tree,
   path: string,
   content: string
-): void => tree.overwrite(path.replace('/', sep), content);
+): void => tree.overwrite(path.replace(/\//g, sep), content);
 
 export const readWorkspaceInTree = (tree: Tree) => readWorkspace(tree);
