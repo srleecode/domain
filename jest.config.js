fb9bd64213489cf2719a16b21a54295ec778847b
Ben Snyder <ben@snyder.tech>
2021-07-02 02:15:56 -0400

    fix jest config and tsconfig for domain package

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
 // file can likely be deleted?
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
-  projects: '<rootDir>/e2e/domain-e2e',
+  projects: [
+    '<rootDir>/packages/domain',
+    '<rootDir>/e2e/domain-e2e',
+  ],
 };
