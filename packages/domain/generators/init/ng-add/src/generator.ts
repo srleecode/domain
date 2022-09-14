ea2e05217b41df46c9323a2b7974f062f3d46f7c
scottlee309 <scottlee309@gmail.com>
2022-09-14 23:32:21 +1000

    chore: remove-install-of-cypress-and-angular

--- packages/domain/generators/init/ng-add/src/generator.ts
+++ packages/domain/generators/init/ng-add/src/generator.ts
@@ -1,20 +1,6 @@
-import {
-  Tree,
-  convertNxGenerator,
-  addDependenciesToPackageJson,
-  installPackagesTask,
-} from '@nrwl/devkit';
+import { Tree, convertNxGenerator, installPackagesTask } from '@nrwl/devkit';
 
 export async function ngAddGenerator(tree: Tree) {
-  const nrwlVersion = '13.8.8';
-  addDependenciesToPackageJson(
-    tree,
-    {},
-    {
-      '@nrwl/angular': nrwlVersion,
-      '@nrwl/cypress': nrwlVersion,
-    }
-  );
   return () => {
     installPackagesTask(tree);
   };
