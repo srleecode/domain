62d42ec3637bb7d9c3eeb447f3030e7b4a97112c
scottlee309 <scottlee309@gmail.com>
2022-09-21 00:43:54 +1000

    feat: add jest junit reporter config

--- packages/domain/generators/front-end/angular/domain-layer/src/schema.d.ts
+++ packages/domain/generators/front-end/angular/domain-layer/src/schema.d.ts
@@ -3,4 +3,5 @@ import { AngularCreateLibrarySchema } from '@srleecode/domain/front-end/shared';
 export interface CreateDomainLayerGeneratorSchema
   extends AngularCreateLibrarySchema {
   groupingFolder: string;
+  addJestJunitReporter?: boolean;
 }
