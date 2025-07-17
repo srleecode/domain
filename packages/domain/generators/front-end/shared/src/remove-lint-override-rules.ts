98e089a970f8dca5398515c0b4436e76dadc21fa
srleecode <srlee309@gmail.com>
2025-07-17 21:52:29 +1000

    chore: fix eslint doesn't use json

--- packages/domain/generators/front-end/shared/src/remove-lint-override-rules.ts
+++ packages/domain/generators/front-end/shared/src/remove-lint-override-rules.ts
@@ -2,8 +2,10 @@ import { readProjectConfiguration, Tree, updateJson } from '@nx/devkit';
 
 export const removeLintOverrideRules = (tree: Tree, projectName: string) => {
   const projectConfig = readProjectConfiguration(tree, projectName);
-  updateJson(tree, `${projectConfig.root}/.eslintrc.json`, (json) => {
-    json.overrides = [];
-    return json;
-  });
+  if (tree.exists(`${projectConfig.root}/.eslintrc.json`)) {
+    updateJson(tree, `${projectConfig.root}/.eslintrc.json`, (json) => {
+      json.overrides = [];
+      return json;
+    });
+  }
 };
